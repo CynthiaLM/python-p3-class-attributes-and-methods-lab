c074f928b536dc619cafea6ad7353506a1475621
Laleti Cynthia <+>
2023-10-18 13:53:21 +0300

    lab submission

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,45 @@
 class Song:
-    pass
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {} 
+    count = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
+
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+        Song.add_song_to_count()
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count = cls.count + increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
